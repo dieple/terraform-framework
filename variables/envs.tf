523c58eb5d17f537e44707aec1b308900021abe2
dieple <dieple1@gmail.com>
2020-03-27 11:50:52 +0000

    update shared arns

--- variables/envs.tf
+++ variables/envs.tf
@@ -8,7 +8,7 @@ variable "envs" {
       account_id         = "111122223333"
       account            = "Dataops Dev Environment"
       workspace_iam_role = "arn:aws:iam::111122223333:role/administrator"
-      share_r53_iam_role = "arn:aws:iam::665085774436:role/administrator"
+      share_r53_iam_role = "arn:aws:iam::000000000000:role/administrator"
       region             = "eu-west-1"
       region_code        = "ew1"
       bucket_region      = "eu-west-1"
@@ -20,7 +20,7 @@ variable "envs" {
       account_id         = "222233334444"
       account            = "DataOps Staging Environment"
       workspace_iam_role = "arn:aws:iam::222233334444:role/administrator"
-      share_r53_iam_role = "arn:aws:iam::665085774436:role/administrator"
+      share_r53_iam_role = "arn:aws:iam::000000000000:role/administrator"
       region_code        = "ew1"
       region             = "eu-west-1"
       bucket_region      = "eu-west-1"
@@ -32,7 +32,7 @@ variable "envs" {
       account_id         = "333344445555"
       account            = "DataOps Production Environment"
       workspace_iam_role = "arn:aws:iam::333344445555:role/administrator"
-      share_r53_iam_role = "arn:aws:iam::665085774436:role/administrator"
+      share_r53_iam_role = "arn:aws:iam::000000000000:role/administrator"
       region_code        = "ew1"
       region             = "eu-west-1"
       bucket_region      = "eu-west-1"
@@ -44,13 +44,14 @@ variable "envs" {
       account_id         = "444455556666"
       account            = "CloudOps Dev Environment"
       workspace_iam_role = "arn:aws:iam::444455556666:role/administrator"
-      share_r53_iam_role = "arn:aws:iam::665085774436:role/administrator"
+      share_r53_iam_role = "arn:aws:iam::000000000000:role/administrator"
       region_code        = "ew1"
       region             = "eu-west-1"
       bucket_region      = "eu-west-1"
       bucket             = "cloudops-dev-tf-infra-state"
       dynamodb           = "cloudops-dev-tf-infra-state-lock"
     }
+
 
 
   }
